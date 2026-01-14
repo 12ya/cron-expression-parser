a5f7ec06602f8a276ccc7a2afd02de1508e888ff
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-01-14 22:55:48 +0900

    add test

---
+++ cmd/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_parse(t *testing.T) {
+	tests := []struct {
+		name    string
+		str     string
+		min     int
+		max     int
+		want    []int
+		wantErr bool
+	}{
+		{
+			name: "empty",
+			str:  "",
+			min:  0,
+			max:  59,
+			want: nil,
+		},
+		{
+			name: "single",
+			str:  "1",
+			min:  0,
+			max:  59,
+			want: []int{1},
+		},
+		{
+			name: "range",
+			str:  "1-5",
+			min:  0,
+			max:  59,
+			want: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "range with step",
+			str:  "1-5/2",
+			min:  0,
+			max:  59,
+			want: []int{1, 3, 5},
+		},
+		{
+			name:    "invalid range",
+			str:     "1-",
+			min:     0,
+			max:     59,
+			wantErr: true,
+		},
+		{
+			name:    "invalid step",
+			str:     "1/2/3",
+			min:     0,
+			max:     59,
+			wantErr: true,
+		},
+		{
+			name:    "invalid step",
+			str:     "1-5/2/3",
+			min:     0,
+			max:     59,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parse(tt.str, tt.min, tt.max)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
