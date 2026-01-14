c52540e6639a0fb8cf2471670475ea01f14c683e
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-01-14 20:46:56 +0900

    add run func that parses and returns the output

--- main.go
+++ main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("Usage: go run . <cron expression>", os.Args[0])
+	}
+
+	cronExpression := os.Args[1]
+	if cronExpression == "" {
+		return fmt.Errorf("cron expression is empty")
+	}
+
+	fmt.Println(cronExpression, "-----expresssion-----")
+
+	parts := strings.Fields(cronExpression)
+
+	minutes, err := parse(parts[0], 0, 59)
+	if err != nil {
+		return fmt.Errorf("parse minutes: %s", err)
+	}
+
+	fmt.Println(minutes)
+
+	return nil
+}
+
+func parse(period string, min, max int) ([]int, error) {
+	return nil, nil
+}
